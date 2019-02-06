9a275f0865c7ffe551c0ef095ab0e7db3d26f70d
yourash <y.anton.ua@gmail.com>
2019-02-06 20:17:50 +0200

    Change text under photo

--- handlers.py
+++ handlers.py
@@ -7,9 +7,7 @@ import config
 class Handler:
 
     def __init__(self, app):
-        aiohttp_jinja2.setup(app,
-                             loader=jinja2.FileSystemLoader(config.TEMPLATES_FOLDER),
-                             )
+        aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader(config.TEMPLATES_FOLDER))
 
     @aiohttp_jinja2.template('index.html')
     async def handle_index(self, request):
